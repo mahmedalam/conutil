cd06e70d0d3b977a18a17a1d0964baf2b0661570
Ahmed Alam <mahmedalamdev@gmail.com>
2026-03-11 02:42:53 +0500

    refactor(ui): update header, hero style and text

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,19 @@ export default function Home() {
   return (
     <main className="min-h-screen container mx-auto flex flex-col items-center gap-11 px-4 py-11 md:p-14">
       {/* Hero Section */}
-      <section className="flex flex-col gap-4 text-center max-w-4xl">
-        <h1>Bulk Image Compressor That Runs in Your Browser</h1>
-        <p>Compress thousands of images locally. No uploads. No tracking.</p>
+      <section className="flex flex-col gap-4">
+        <h1>
+          IN BROWSER
+          <br />
+          BULK IMAGE
+          <br />
+          COMPRESSOR
+        </h1>
+        <p>
+          Your private image compressor, auto resizer, and format converter, all
+          in one toolkit to boost website conversions or save cloud and device
+          storage.
+        </p>
       </section>
       {/* Upload Box */}
       <section>
